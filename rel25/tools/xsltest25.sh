5d93423cda9bacd95efffaeeb60ebd8dfca4b4c7
aquinn <aquinn>
2006-03-10 17:19:41 +0000

    Use ../../xml/lib/saxon.jar (../lib/saxon.jar is corrupt).

--- rel25/tools/xsltest25.sh
+++ rel25/tools/xsltest25.sh
@@ -1,11 +1,21 @@
-#!/bin/sh -x
-#
-echo "Transforming files"
-mkdir -p tmp
-XSLT='java -jar lib/saxon.jar '
-cp sampleData/$1.xml tmp
-$XSLT tmp/$1.xml         tools/MIF25_view.xsl    > tmp/$1.html
-$XSLT tmp/$1.xml         tools/MIF25_compact.xsl > tmp/$1-compact.xml
-$XSLT tmp/$1-compact.xml tools/MIF25_expand.xsl  > tmp/$1-expand.xml
+# Transform PSI-MI XML file to compact, expanded and HTML
+# Pass in ID of XML file in ../sampleData
+# Example: ./xsltest25.sh 10523676
 
-# end
+export tmp=/tmp/psi
+mkdir -p $tmp
+cp ../sampleData/$1.xml $tmp
+
+xslt='java -jar ../../xml/lib/saxon.jar '
+
+echo "Generating $1.html..."
+$xslt $tmp/$1.xml         MIF25_view.xsl    > $tmp/$1.html
+
+echo "Generating $1-compact.xml..."
+$xslt $tmp/$1.xml         MIF25_compact.xsl > $tmp/$1-compact.xml
+
+echo "Generating $1-expand.xml..."
+$xslt $tmp/$1-compact.xml MIF25_expand.xsl  > $tmp/$1-expand.xml
+
+echo "See:"
+ls -lh  $tmp/$1*
